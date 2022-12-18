1fcdc110285209be90d889aa3f73647742b3b442
Pablo Ungaro <ungaro.pablo@gmail.com>
2022-12-18 14:52:24 -0300

    extracting api base url

--- src/main/java/com/br/membership/repository/ECoreApiRepository.java
+++ src/main/java/com/br/membership/repository/ECoreApiRepository.java
@@ -4,6 +4,7 @@ import com.br.membership.dto.output.TeamDto;
 import com.br.membership.dto.output.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.client.RestTemplate;
@@ -16,17 +17,20 @@ import java.util.Optional;
 public class ECoreApiRepository implements ECoreRepository {
     private final RestTemplate restTemplate;
 
+    @Value("${e-core.api.base-url}")
+    private String eCoreBaseUrl;
+
     @Override
     @Cacheable("eCoreUser")
     public Optional<UserDto> findUserById(String id) {
         log.info("Getting user %s".formatted(id));
-        return Optional.ofNullable(restTemplate.getForObject("https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/users/%s".formatted(id), UserDto.class));
+        return Optional.ofNullable(restTemplate.getForObject("%s/users/%s".formatted(eCoreBaseUrl, id), UserDto.class));
     }
 
     @Override
     @Cacheable("eCoreTeam")
     public Optional<TeamDto> findTeamById(String id) {
         log.info("Getting team %s".formatted(id));
-        return Optional.ofNullable(restTemplate.getForObject("https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/%s".formatted(id), TeamDto.class));
+        return Optional.ofNullable(restTemplate.getForObject("%s/teams/%s".formatted(eCoreBaseUrl, id), TeamDto.class));
     }
 }
--- src/test/java/com/br/membership/StringUtilTests.java
+++ src/test/java/com/br/membership/StringUtilTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MembershipApplicationTests {
+class StringUtilTests {
 
     @Test
-    void contextLoads() {
+    void sanitizeTest() {
+
     }
 
 }
